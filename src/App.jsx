d5983a26be97b3864338e5567c027a326475fe35
Alan Rodriguez <gheryk90@gmail.com>
2024-06-01 09:38:36 +0100

    Basic styles / Input & NewProject components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import ProjectSideBar from "./components/ProjectSideBar";
 function App() {
   return (
-    <>
+    <main className="h-screen my-8">
       <ProjectSideBar/>
-    </>
+    </main>
   );
 }
 
