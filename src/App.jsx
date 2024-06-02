4960f4e1de2cc327d0ff8694d3d5458d6a3cdee8
Alan Rodriguez <gheryk90@gmail.com>
2024-06-02 09:35:12 +0100

    save and condition to display

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,54 @@
+import { useState } from 'react';
+
 import ProjectSideBar from "./components/ProjectSideBar";
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import NoWorkResult from 'postcss/lib/no-work-result';
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null
+      }
+    });
+  }
+
+  function handleAddProject(projectData) {
+    setProjectsState(prevState => {
+      const projectId = Math.random();
+      const newProject = {
+        ...projectData,
+        id: projectId
+      }
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject]
+      }
+    })
+  }
+
+  console.log(projectsState)
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSideBar/>
-      <NewProject />      
+      <ProjectSideBar onStartAddProject={handleStartAddProject} projects={projectsState.projects}/>
+      {content}
     </main>
   );
 }
