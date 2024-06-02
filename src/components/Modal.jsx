bb57fc8253e59fad4000403939f9406f7deadcaa
Alan Rodriguez <gheryk90@gmail.com>
2024-06-02 09:47:18 +0100

    modal with validation

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,25 @@
-export default function Modal({children}) {
-  return (
-    <dialog>
+import { createPortal } from "react-dom"
+import { forwardRef, useImperativeHandle, useRef } from "react";
+
+const Modal = forwardRef(function Modal({children, buttonCaption}, ref) {
+  const dialog = useRef();
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      }
+    };
+  });
+
+  return createPortal(
+    <dialog ref={dialog}>
       {children}
-    </dialog>
-  )
-}
+      <form action="dialog">
+        <button>{buttonCaption}</button>
+      </form>
+    </dialog>,
+    document.getElementById('modal-root')
+  );
+});
+
+export default Modal
