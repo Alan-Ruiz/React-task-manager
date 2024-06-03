0f264da432ccde74f9547501599a2e2ad1b0a5dc
Alan Rodriguez <gheryk90@gmail.com>
2024-06-03 21:58:08 +0100

    Selected project show and delete

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,22 @@ import ProjectSideBar from "./components/ProjectSideBar";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import NoWorkResult from 'postcss/lib/no-work-result';
+import SelectedProject from './components/SelectedProject';
 
 function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
     projects: []
   });
+
+  function handleSelectProject(id) {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: id
+      }
+    });
+  }
 
   function handleStartAddProject() {
     setProjectsState(prevState => {
@@ -44,9 +54,19 @@ function App() {
     })
   }
 
-  console.log(projectsState)
+  function handleDeleteProject() {
+    setProjectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: prevState.projects.filter((project) => project.id !== prevState.selectedProjectId)
+      }
+    });
+  }
 
-  let content;
+  const selectedProject = projectsState.projects.find(project => project.id === projectsState.selectedProjectId);
+
+  let content = <SelectedProject project={selectedProject} onDelete={handleDeleteProject}/>
 
   if (projectsState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
@@ -56,7 +76,12 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSideBar onStartAddProject={handleStartAddProject} projects={projectsState.projects}/>
+      <ProjectSideBar 
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+        onSelectProject={handleSelectProject}
+      
+      />
       {content}
     </main>
   );
